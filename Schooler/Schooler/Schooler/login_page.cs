233e5dd3ade9d66799818ae42c2de944149c7947
GooSeyoung <0917sy@gmail.com>
2016-12-06 21:25:21 +0900

    자마린 프로젝트 생성 로긴페이지 생성

---
+++ Schooler/Schooler/Schooler/login_page.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Schooler
+{
+    public class login_page : ContentPage
+    {
+        public login_page()
+        { 
+            var loginBtn = new Button();
+            loginBtn.Text = "Sign In";
+            loginBtn.Clicked += LoginBtn_Clicked;
+
+            var signupBtn = new Button() {
+                Text = "Sign Up"
+            };
+            signupBtn.Clicked += SignupBtn_Clicked;
+
+            var idLb = new Label()
+            {
+                Text = "ID"
+            };
+            var id = new Entry()
+            {
+                Placeholder = "ID"
+            };
+            var pw = new Entry()
+            {
+                Placeholder = "Password",
+                IsPassword = true
+            };
+           
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = "Login Page",
+
+                         HorizontalOptions = LayoutOptions.Center
+                    },
+                    id,
+                    pw,
+                    loginBtn,
+                    signupBtn
+
+          
+                }
+            };
+
+            Padding = new Thickness(10, 10, 10, 10);
+            
+        }
+
+        private void SignupBtn_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void LoginBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
