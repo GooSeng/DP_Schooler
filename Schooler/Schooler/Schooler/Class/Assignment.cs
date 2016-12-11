c7f5b63d786503c28155f19590097addc5cc9c32
WonJunLee <dldnjs1013@nate.com>
2016-12-11 17:10:31 +0900

    dao추가

--- Schooler/Schooler/Schooler/Class/Assignment.cs
+++ Schooler/Schooler/Schooler/Class/Assignment.cs
@@ -8,16 +8,36 @@ namespace Schooler.Class
 {
     class Assignment
     {
-        private string name;
-        private DateTime deadline;
-        private List<File> fileList;
-        private List<Comment> commentList;
+        protected string name;
+        protected DateTime deadline;
+        protected List<File> fileList;
+        protected List<Comment> commentList;
         protected int idx;
-        //       protected IDao Dao;
+        protected IDao dao;
+
+        public Assignment(int idx, string name, DateTime deadline)
+        {
+            this.idx = idx;
+            this.name = name;
+            this.deadline = deadline;
+
+        }
+
+        protected virtual void Init()
+        {
+            fileList = Dao().GetFileList();
+            commentList = Dao().GetCommentList();
+        }
 
         public bool settingAssignment(string name, DateTime date)
         {
             return true;
+        }
+
+        protected virtual AssignmentDao Dao()
+        {
+            dao = new AssignmentDao(idx);
+            return (dao as AssignmentDao);
         }
     }
 }
---
+++ Schooler/Schooler/Schooler/Class/AssignmentDao.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schooler.Class
+{
+    class AssignmentDao : IDao
+    {
+        protected int idx;
+        protected HttpClient client;
+        protected string baseUrl = "https://schoolerAPIServer.azurewebsites.net/api/";
+
+        public AssignmentDao(int idx)
+        {
+            this.idx = idx;
+        }
+
+        //-----------------------파일 관련-----------------------//
+        //---------------------------------------------------------//
+
+        //public bool UploadFile(File file)
+        //{
+        //    // TODO implement here
+        //    return False;
+        //}
+
+
+        public List<File> GetFileList()
+        {
+            // TODO implement here
+            return null;
+        }
+
+        //-----------------------코멘트 관련-----------------------//
+        //---------------------------------------------------------//
+
+        //public bool UploadComment(Comment comment)
+        //{
+        //    // TODO implement here
+        //    return False;
+        //}
+
+
+        public List<Comment> GetCommentList()
+        {
+            // TODO implement here
+            return null;
+        }
+
+        //public bool DeleteComment(int commentIdx)
+        //{
+        //    // TODO implement here
+        //    return False;
+        //}
+
+
+        //-----------------------과제 관련-----------------------//
+        //---------------------------------------------------------//
+        //public bool DeleteAssigment(int objIdx)
+        //{
+        //    // TODO implement here
+        //    return false;
+        //}
+
+
+    }
+}
