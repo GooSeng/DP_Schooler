c84600db66910b689d361c787d9f12f49a112e7c
WonJunLee <dldnjs1013@nate.com>
2016-12-13 03:16:37 +0900

    택긇파일

--- Schooler/Schooler/Schooler/Views/ProjectItemPage.cs
+++ Schooler/Schooler/Schooler/Views/ProjectItemPage.cs
@@ -14,8 +14,18 @@ namespace Schooler.Views
 		int idx;
 		ProjectDao dao;
 		ScrollView scrollView;
-		
-		public ProjectItemPage(int _idx)
+
+        ListView fileList;
+        Button fileAddBtn;
+        Entry fileUrlEntry;
+        StackLayout fileLayout;
+
+        ListView commentList;
+        Button commentAddBtn;
+        Entry commentEntry;
+        StackLayout commentLayout;
+
+        public ProjectItemPage(int _idx)
 		{
 			NavigationPage.SetHasNavigationBar(this, true);
 			idx = _idx;
@@ -47,75 +57,77 @@ namespace Schooler.Views
 			if (idx != -1)
 				isTeamSwitch.IsEnabled = false;
 
-			// File List 
-			var fileList = new ListView
-			{
-				HeightRequest = 100,
-				RowHeight = 40,
-			ItemTemplate = new DataTemplate(typeof(Views.FileCell))
-			};
-			fileList.SetBinding(ListView.ItemsSourceProperty, "fileList");
-			var fileAddBtn = new Button { Text = "+", WidthRequest = 30, HeightRequest = 30, Margin = 0 };
-			fileAddBtn.Clicked += FileAddBtn_Clicked;
+            // File List 
+            fileList = new ListView
+            {
+                RowHeight = 40,
+                ItemTemplate = new DataTemplate(typeof(Views.FileCell))
+            };
+            fileList.ItemTemplate.SetBinding(FileCell.idxProperty, "Idx");
+            fileList.SetBinding(ListView.ItemsSourceProperty, "fileList");
+            fileList.ItemsSource = dao.GetFileList();
+            fileUrlEntry = new Entry { Placeholder = "URL", WidthRequest = 100, HeightRequest = 30 };
+            fileAddBtn = new Button { Text = "+", WidthRequest = 30, HeightRequest = 30, Margin = 0 };
+            fileAddBtn.Clicked += FileAddBtn_Clicked;
 
-			var fileLayout = new StackLayout
-			{
-				Orientation = StackOrientation.Vertical,
-				Children =
-				{
-					new StackLayout
-					{
-						VerticalOptions = LayoutOptions.Center,
-						Orientation = StackOrientation.Horizontal,
-						Children =
-						{
-							new Label { Text = "File list" },
-							fileAddBtn
-						}
-					},
-					new StackLayout
-					{
-						VerticalOptions = LayoutOptions.Fill,
-						HorizontalOptions = LayoutOptions.Fill,
-						Children = { fileList }
-					}
-				}
-			};
+            fileLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    new Label { Text = "File list" },
+                    fileList,
+                    new StackLayout
+                    {
+                        VerticalOptions = LayoutOptions.Center,
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            fileUrlEntry,
+                            fileAddBtn
+                        }
+                    }
 
-			// Comment List
-			var commentList = new ListView
-			{
-				HeightRequest = 100,
-				RowHeight = 40,
-				ItemTemplate = new DataTemplate(typeof(Views.CommentCell))
-			};
-			commentList.SetBinding(ListView.ItemsSourceProperty, "commentList");
-			var commentAddBtn = new Button { Text = "+", WidthRequest = 30, HeightRequest = 30 };
-			var commentEntry = new Entry { WidthRequest = 300 };
-			commentAddBtn.Clicked += CommentAddBtn_Clicked;
+                }
+            };
 
-			var commentLayout = new StackLayout
-			{
-				Orientation = StackOrientation.Vertical,
-				Children =
-				{
-					new Label { Text = "Comment list" },
-					new StackLayout
-					{
-						VerticalOptions = LayoutOptions.Center,
-						Orientation = StackOrientation.Horizontal,
-						Children =
-						{
-							commentEntry,
-							commentAddBtn
-						}
-					},
-					commentList,
-				}
-			};
+            // Comment List
+            commentList = new ListView
+            {
+                RowHeight = 40,
+                ItemTemplate = new DataTemplate(typeof(Views.CommentCell))
+            };
+            commentList.ItemTemplate.SetBinding(CommentCell.idxProperty, "Idx");
+            commentList.SetBinding(ListView.ItemsSourceProperty, "commentList");
+            commentList.ItemsSource = dao.GetCommentList();
+            commentAddBtn = new Button { Text = "+", WidthRequest = 30, HeightRequest = 30 };
+            commentEntry = new Entry { WidthRequest = 300 };
+            commentEntry.SetBinding(Entry.TextProperty, "comment");
+            commentAddBtn.Clicked += CommentAddBtn_Clicked;
 
-			// Team List
-			var teamList = new ListView
+            commentLayout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children =
+                {
+                    commentList,
+                    new Label { Text = "Comment list" },
+                    new StackLayout
+                    {
+                        VerticalOptions = LayoutOptions.Center,
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            commentEntry,
+                            commentAddBtn
+                        }
+                    }
+
+                }
+            };
+
+            // Team List
+            var teamList = new ListView
 			{
 				HeightRequest = 100,
 				RowHeight = 40,
@@ -201,15 +213,7 @@ namespace Schooler.Views
 			Content = scrollView;
 		}
 		
-		private void FileAddBtn_Clicked(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
-		}
-
-		private void CommentAddBtn_Clicked(object sender, EventArgs e)
-		{
-			throw new NotImplementedException();
-		}
+	
 
 		private void TeamAddBtn_Clicked(object sender, EventArgs e)
 		{
@@ -243,5 +247,34 @@ namespace Schooler.Views
 		{
 			await Navigation.PopAsync();
 		}
-	}
+
+        private void CommentAddBtn_Clicked(object sender, EventArgs e)
+        {
+            UserDao userdao = new UserDao();
+            var item = new Comment { comment = commentEntry.Text };
+            item.uploadUserId = userdao.GetLoginedUser();
+            item.projectIdx = idx;
+
+            dao.UploadComment(item);
+
+        }
+
+        private async void FileAddBtn_Clicked(object sender, EventArgs e)
+        {
+            UserDao userdao = new UserDao();
+            var item = new File();
+            item.uploadUserId = userdao.GetLoginedUser();
+            item.projectIdx = idx;
+            item.name = fileUrlEntry.Text;
+            item.url = item.name;
+
+
+            bool isOk = await dao.UploadFile(item);
+
+            if (isOk)
+                await DisplayAlert("Good", "File Upload", "OK");
+            else
+                await DisplayAlert("Error", "File size error, Please Check the File Size", "Ok");
+        }
+    }
 }
